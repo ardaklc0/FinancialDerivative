9a7b51adf3bdeaca8da5e8a7252a15f877845cb9
arda <ardaklc0@gmail.co>
2023-12-12 16:23:55 +0300

    login page is fixed, when I enter url localhost:8080/login the code redirect me to the login page.

--- src/main/java/com/test/demo/controller/HomeController.java
+++ src/main/java/com/test/demo/controller/HomeController.java
@@ -10,22 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-    private final UserDetailsService userService;
-    @GetMapping("/")
-    public String home(Model model, Authentication authentication){
-        if (authentication != null){
-            User user = userService.loadUserByUsername()
-        }
-    }
-
-    @RequestMapping("/login.html")
+    @GetMapping("/login")
     public String login(){
         return "login";
     }
 
-    @RequestMapping("/login-error.html")
-    public String loginError(Model model){
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
         return "login-error";
     }
 }
