b3805310d782eae041b324bc8ded7d43f3b7c9dc
ardkili <arda.kilic@mercedes-benz.com>
2023-12-18 15:42:08 +0300

    Login issue has been solved. Now, user can easily sign in and sign up.

--- src/main/java/com/test/demo/config/SecurityConfiguration.java
+++ src/main/java/com/test/demo/config/SecurityConfiguration.java
@@ -35,13 +35,14 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authConfig -> {
-                authConfig.requestMatchers(HttpMethod.GET, "/", "/register", "/login", "/error", "/login-error", "/logout", "/css/**").permitAll();
+                authConfig.requestMatchers(HttpMethod.GET, "/index", "/register", "/login", "/error", "/login-error", "/logout", "/css/**").permitAll();
+                authConfig.requestMatchers(HttpMethod.POST, "/save").permitAll();
                 authConfig.requestMatchers(HttpMethod.GET, "/user").hasRole("USER");
                 authConfig.requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN");
             })
             .formLogin(login -> {
                         login.loginPage("/login");
-                        login.defaultSuccessUrl("/");
+                        login.defaultSuccessUrl("/index");
                         login.failureUrl("/login-error");
             })
             .logout(logout -> {
--- src/main/java/com/test/demo/model/User.java
+++ src/main/java/com/test/demo/model/User.java
@@ -72,5 +72,4 @@ public class User {
                         "\nuser_role=" + roles +
                         "}\n";
     }
-
 }
