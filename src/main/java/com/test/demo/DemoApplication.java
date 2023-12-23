48a2334259d2b64a627f6675fc3e4d278cbd3e12
ardkili <arda.kilic@mercedes-benz.com>
2023-12-23 13:37:55 +0300

    Toast for deletion has been added. Update Page has been modified. register_failure has been modified, when someone try to register with same username then the user get an error.

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -1,22 +1,24 @@
 package com.test.demo;
 
+import com.test.demo.dto.request.user.CreateNewUserRequest;
 import com.test.demo.model.Role;
 import com.test.demo.model.User;
 import com.test.demo.repository.RoleRepository;
 import com.test.demo.repository.UserRepository;
+import com.test.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
 
