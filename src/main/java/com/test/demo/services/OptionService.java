add1f4e93314180444bfe759bc02b89c45d61d0b
arda <ardaklc0@gmail.co>
2023-12-24 18:17:12 +0300

    Option page has been modified. Now Call Option can be calculated. OptionService now has many financial tools. Same Styling have been added.

--- src/main/java/com/test/demo/services/OptionService.java
+++ src/main/java/com/test/demo/services/OptionService.java
@@ -54,4 +54,42 @@ public class OptionService{
         Optional<Option> optionalOption = optionRepository.findById(id);
         return optionalOption.orElseThrow(() -> new IllegalArgumentException("Invalid option Id:" + id));
     }
+
+    public long calculateUpValue(long stockPrice, double upRate){
+        return (long) (stockPrice * (1 + upRate));
+    }
+    public long calculateDownValue(long stockPrice, double downRate){
+        return (long) (stockPrice * (1 - downRate));
+    }
+    public long calculateFutureBond(long bondPrice, double riskFreeInterestValue){
+        return (long) (bondPrice * (1 + riskFreeInterestValue));
+    }
+    public long calculateUpPortfolio(long stockPrice, long bondPrice, double upRate, double riskFreeInterestValue){
+        return (long) (stockPrice * (1 + upRate) + bondPrice * (1 + riskFreeInterestValue));
+    }
+    public long calculateDownPortfolio(long stockPrice, long bondPrice, double downRate, double riskFreeInterestValue){
+        return (long) (stockPrice * (1 - downRate) + bondPrice * (1 + riskFreeInterestValue));
+    }
+    public long calculateLayOff(long strikePrice, long stockPrice, double upRate){
+        long stockUpPrice = calculateUpValue(stockPrice, upRate);
+        return stockUpPrice - strikePrice;
+    }
+    public double calculateStockShare(long stockPrice, double upRate, double downRate, long strikePrice){
+        long stockUpPrice = calculateUpValue(stockPrice, upRate);
+        long stockDownPrice = calculateDownValue(stockPrice, downRate);
+        return (double) (stockUpPrice - strikePrice) / (stockUpPrice - stockDownPrice);
+    }
+    public double calculateBondShare(long stockPrice, double upRate, double downRate, long StrikePrice, long bondPrice, double riskFreeInterestValue){
+        double stockShare = calculateStockShare(stockPrice, upRate, downRate, StrikePrice);
+        long futureBond = calculateFutureBond(bondPrice, riskFreeInterestValue);
+        long stockDownPrice = calculateDownValue(stockPrice, downRate);
+        return -(stockShare * ((double) stockDownPrice / futureBond));
+    }
+    //TODO 01: Bug Fix
+    public double replicateThePortfolio(long stockPrice, double upRate, double downRate, long StrikePrice, long bondPrice, double riskFreeInterestValue){
+        double stockShare = calculateStockShare(stockPrice, upRate, downRate, StrikePrice);
+        double bondShare = calculateBondShare(stockPrice, upRate, downRate, StrikePrice, bondPrice, riskFreeInterestValue);
+        return stockShare * stockPrice + bondShare * bondPrice;
+    }
+
 }
