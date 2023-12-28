451c44b9365c197d8fefbab24b314539aa3f4a5d
ardkili <arda.kilic@mercedes-benz.com>
2023-12-28 18:06:26 +0300

    Annuity functions have been added. TODO must be controlled.

---
+++ src/main/java/com/test/demo/controller/AnnuityController.java
@@ -0,0 +1,60 @@
+package com.test.demo.controller;
+
+import com.test.demo.dto.request.annuity.CreateNewAnnuityRequest;
+import com.test.demo.dto.request.annuity.UpdateExistingAnnuityRequest;
+import com.test.demo.model.Annuity;
+import com.test.demo.services.AnnuityService;
+import com.test.demo.viewmodels.AnnuityModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/annuity")
+public class AnnuityController {
+    @Autowired
+    private AnnuityService annuityService;
+    @GetMapping("/annuity-home")
+    public String annuity(Model model, Authentication authentication){
+        model.addAttribute("user", authentication.getPrincipal());
+        return "annuity/annuity";
+    }
+    @GetMapping("/create-annuity")
+    public String showCreateAnnuityForm(Model model){
+        model.addAttribute("annuity", new CreateNewAnnuityRequest());
+        return "annuity/create_annuity";
+    }
+    @PostMapping("/create-annuity")
+    public String createAnnuity(@ModelAttribute("annuity") CreateNewAnnuityRequest annuity, Model model){
+        Annuity newAnnuity = annuityService.save(annuity);
+        AnnuityModel annuityModel = new AnnuityModel(newAnnuity, annuityService);
+        Map<String, Object> annuityValues = annuityModel.calculateAllValues();
+        model.addAttribute("annuityValues", annuityValues);
+        return "annuity/annuity_success";
+    }
+    @GetMapping("/update-annuity/{id}")
+    public String showUpdateAnnuityForm(@PathVariable("id") long id, Model model){
+        Annuity annuity = annuityService.findById(id);
+        model.addAttribute("annuity", annuity);
+        return "annuity/update_annuity";
+    }
+    @PostMapping("/update-annuity/{id}")
+    public String updateAnnuity(@PathVariable("id") long id, UpdateExistingAnnuityRequest annuity, BindingResult result, Model model){
+        if (result.hasErrors()) {
+            annuity.setId(id);
+            return "annuity/update_annuity";
+        }
+        annuityService.update(annuity, id);
+        Annuity newAnnuity = annuityService.findById(id);
+        AnnuityModel annuityModel = new AnnuityModel(newAnnuity, annuityService);
+        Map<String, Object> annuityValues = annuityModel.calculateAllValues();
+        model.addAttribute("annuity", newAnnuity);
+        model.addAttribute("annuityValues", annuityValues);
+        return "annuity/annuity_success";
+    }
+}
--- src/main/java/com/test/demo/model/Option.java
+++ src/main/java/com/test/demo/model/Option.java
@@ -27,7 +27,6 @@ public class Option {
     private Long bondPrice;
     @Column(name = "probability_of_up", nullable = true)
     private Double probabilityOfUp;
-
     public Option(boolean isCall, Long strikePrice, Double riskFreeInterestValue, Double upRate, Double downRate, int exerciseTime, Long stockPrice, Long bondPrice, Double probabilityOfUp){
         this.isCall = isCall;
         this.strikePrice = strikePrice;
@@ -40,85 +39,64 @@ public class Option {
         this.probabilityOfUp = probabilityOfUp;
     }
     public Option(){}
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long option_id) {
         this.id = option_id;
     }
-
     public boolean getIsCall() {
         return isCall;
     }
-
     public void setIsCall(boolean option_is_call) {
         this.isCall = option_is_call;
     }
-
     public Long getStrikePrice() {
         return strikePrice;
     }
-
     public void setStrikePrice(Long option_strike_price) {
         this.strikePrice = option_strike_price;
     }
-
     public Double getRiskFreeInterestValue() {
         return riskFreeInterestValue;
     }
-
     public void setRiskFreeInterestValue(Double optionRiskFreeInterestValue) {
         this.riskFreeInterestValue = optionRiskFreeInterestValue;
     }
-
     public Double getUpRate() {
         return upRate;
     }
-
     public void setUpRate(Double option_up_rate) {
         this.upRate = option_up_rate;
     }
-
     public Double getDownRate() {
         return downRate;
     }
-
     public void setDownRate(Double option_down_rate) {
         this.downRate = option_down_rate;
     }
-
     public int getExerciseTime() {
         return exerciseTime;
     }
-
     public void setExerciseTime(int exerciseTime) {
         this.exerciseTime = exerciseTime;
     }
-
     public Long getStockPrice() {
         return stockPrice;
     }
-
     public void setStockPrice(Long option_stock_price) {
         this.stockPrice = option_stock_price;
     }
-
     public Long getBondPrice() {
         return bondPrice;
     }
-
     public void setBondPrice(Long option_bond_price) {
         this.bondPrice = option_bond_price;
     }
-
     public Double getProbabilityOfUp() {
         return probabilityOfUp;
     }
-
     public void setProbabilityOfUp(Double option_probability_of_up) {
         this.probabilityOfUp = option_probability_of_up;
     }
-
 }
